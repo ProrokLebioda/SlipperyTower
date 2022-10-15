7f228faaa2c25d3e197c9601b0742e60d853a332
DESKTOP-99HRA0O\Patryk <komosa.patryk@gmail.com>
2022-10-15 22:50:11 +0200

    WI05: Add level streaming Added random platform placement. Early works, not clean, not working well, need to add walls placement. Currently spacing and size of platforms is hardcoded.

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -6,22 +6,94 @@ using UnityEngine.Tilemaps;
 
 public class LevelGenerator : MonoBehaviour
 {
-    
+
+    private int leftMostX = -5;
+    private int rightMostX = 4;
+    private int bottomY = 0;
+
+    public int sectionHeight = 20;
+
+    private int testLevels = 200;
+
     public Tilemap platformTilemap;
     public RuleTile ruleTile;
     void Start()
     {
         GeneratePlatforms();
+
+
     }
 
     private void GeneratePlatforms()
     {
-        platformTilemap.SetTile(new Vector3Int(0, 0, 0), ruleTile);
+        int horizontalSpacing = 1; // 1 means every tile
+        // test generate for 20 levels
+        // from leftMostX and bottomY to rightMostX and sectionHeight
+        //for (int y = bottomY; y < sectionHeight; y++)
+        for (int y = bottomY; y < testLevels; y++)
+        {
+            // Section division deduce 1 to make it work
+            if (y > 0 && y % (sectionHeight-1) == 0)
+            {
+                horizontalSpacing = 1;
+                for (int x = leftMostX; x <= rightMostX; x += horizontalSpacing)
+                {
+                    platformTilemap.SetTile(new Vector3Int(x, y, 0), ruleTile);
+                }
+            }
+            else
+            {
+                // Add some code to handle drawing tiles for normal platforms
+                //
+                PlaceRandomPlatforms(y);
+            }            
+        }
+    }
+
+    private void PlaceRandomPlatforms(int y)
+    {
+        if (y % 2 == 0)
+            return;
+        int horizontalSpacing = 3;
+
+        // for the ease of creating do a 5 tile platforms only, 1 per level
+
+        bool isPlaceable = false;
+
+        int leftEdgeOfPlatform = -100;
+        int platformSize = 5;
+
+        while (!isPlaceable)
+        {
+            leftEdgeOfPlatform = UnityEngine.Random.Range(leftMostX, rightMostX);
+
+            if (leftEdgeOfPlatform+platformSize <= rightMostX)
+            {
+                isPlaceable = true;
+            }
+        }
+
+        for (int x = leftEdgeOfPlatform; x <= leftEdgeOfPlatform+rightMostX; x += 1)
+        {
+            platformTilemap.SetTile(new Vector3Int(x, y, 0), ruleTile);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ShouldGenerate())
+        {
+
+
+
+        }
+    }
+
+    bool ShouldGenerate()
+    {
         
+
+        return true;
     }
 }
