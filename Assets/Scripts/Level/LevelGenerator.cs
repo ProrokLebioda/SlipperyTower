f504fed024e4921918fbb68af56912e4627b01f9
DESKTOP-99HRA0O\Patryk <komosa.patryk@gmail.com>
2022-10-23 12:42:46 +0200

    WI05: Add level streaming pt1 Added level generation Fixed some issues with platform placement (vertically). No longer clumped platforms

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -9,41 +9,75 @@ public class LevelGenerator : MonoBehaviour
 
     private int leftMostX = -5;
     private int rightMostX = 4;
-    private int bottomY = 0;
+    private int bottomY = 1;
 
     public int sectionHeight = 20;
+    private int currentPlayerSection = 0;
+    private int platformSize = 5;
 
-    public int testLevels = 200;
+    public int startSectionLevels = 20;
 
     public Tilemap platformTilemap;
     public Tilemap wallsTilemap;
     public Tilemap collisionWallsTilemap;
     public RuleTile platformRuleTile;
     public RuleTile wallRuleTile;
+
+    public GameObject playerCamera;
+
+    private GameObject player;
     void Start()
     {
         GeneratePlatforms();
-
+        player = GameObject.FindWithTag("Player");
 
     }
 
+    private void FixedUpdate()
+    {
+        // Check player position
+
+        if (player != null)
+        {
+            int playerY = Mathf.RoundToInt(player.transform.position.y);
+            if (playerY > (currentPlayerSection * sectionHeight + (sectionHeight / 2)))
+            {
+                
+                //workswonky for some reason...
+                GeneratePlatforms((sectionHeight + 1) + sectionHeight * (currentPlayerSection), (sectionHeight) + sectionHeight * (currentPlayerSection + 1));
+                currentPlayerSection++;
+                if (platformSize >= 4 && currentPlayerSection % 5 == 0)
+                    platformSize--;
+
+                if (currentPlayerSection % 3 == 0)
+                {
+                    playerCamera.GetComponent<FollowCamera>().cameraSpeed += 0.2f;
+                }
+            }
+
+            //if (playerY > currentPlayerSection * sectionHeight + sectionHeight)
+            //{
+               
+            //}
+        }
+    }
+
+
     private void GeneratePlatforms()
     {
-        int horizontalSpacing = 1; // 1 means every tile
         // test generate for 20 levels
         // from leftMostX and bottomY to rightMostX and sectionHeight
         //for (int y = bottomY; y < sectionHeight; y++)
-        for (int y = bottomY; y <= testLevels; y++)
+        for (int y = bottomY; y <= sectionHeight; y++)
         {
             for (int x = leftMostX - 4; x < leftMostX; x++)
             {
                 wallsTilemap.SetTile(new Vector3Int(x, y, 0), wallRuleTile);
             }
 
-            // Section division deduce 1 to make it work
+            //Section division
             if (y > 0 && y % (sectionHeight) == 0)
             {
-                horizontalSpacing = 1;
                 for (int x = leftMostX; x <= rightMostX; x++)
                 {
                     platformTilemap.SetTile(new Vector3Int(x, y, 0), platformRuleTile);
@@ -63,18 +97,51 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
+    private void GeneratePlatforms(int fromY,int toY)
+    {
+        // test generate for 20 levels
+        // from leftMostX and bottomY to rightMostX and sectionHeight
+        //for (int y = bottomY; y < sectionHeight; y++)
+        for (int y = fromY; y <= toY; y++)
+        {
+            for (int x = leftMostX - 4; x < leftMostX; x++)
+            {
+                wallsTilemap.SetTile(new Vector3Int(x, y, 0), wallRuleTile);
+            }
+
+            // Section division 
+            if (y > 0 && y % (sectionHeight) == 0)
+            {
+                for (int x = leftMostX; x <= rightMostX; x++)
+                {
+                    platformTilemap.SetTile(new Vector3Int(x, y, 0), platformRuleTile);
+                }
+            }
+            else
+            {
+                // Add some code to handle drawing tiles for normal platforms
+                //
+                PlaceRandomPlatforms(y);
+            }
+
+            for (int x = rightMostX + 1; x <= rightMostX + 4; x++)
+            {
+                wallsTilemap.SetTile(new Vector3Int(x, y, 0), wallRuleTile);
+            }
+        }
+    }
+
     private void PlaceRandomPlatforms(int y)
     {
-        if (y % 2 == 0)
+        if (y % 2 == 1)
             return;
-        int horizontalSpacing = 3;
 
         // for the ease of creating do a 5 tile platforms only, 1 per level
 
         bool isPlaceable = false;
 
         int leftEdgeOfPlatform = -100;
-        int platformSize = 5;
+        
 
         while (!isPlaceable)
         {
@@ -86,7 +153,7 @@ public class LevelGenerator : MonoBehaviour
             }
         }
 
-        for (int x = leftEdgeOfPlatform; x <= leftEdgeOfPlatform+rightMostX; x += 1)
+        for (int x = leftEdgeOfPlatform; x <= leftEdgeOfPlatform + platformSize; x += 1)
         {
             platformTilemap.SetTile(new Vector3Int(x, y, 0), platformRuleTile);
         }
