06e3a191b36d9ff1a94ed8ae7a6be8767b3fa225
DESKTOP-99HRA0O\Patryk <komosa.patryk@gmail.com>
2023-01-02 17:31:04 +0100

    WI11: Level streaming pt2 Fixed level load issues, background issues. Added icon and splash screen. Released 0.9 on itch.io

--- Assets/Scripts/Level/Background.cs
+++ Assets/Scripts/Level/Background.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    private BoxCollider2D boxCollider;
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider;
     [SerializeField]
-    private float backgroundHeight;
+    private float _backgroundHeight;
 
-    private void OnEnable()
+
+    private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.size = new Vector2(spriteRenderer.size.x, backgroundHeight);
-        boxCollider = GetComponent<BoxCollider2D>();
-        boxCollider.offset = new Vector2(boxCollider.offset.x ,Mathf.Ceil(spriteRenderer.size.y / 2));
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.size = new Vector2(_spriteRenderer.size.x, _backgroundHeight);
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _boxCollider.offset = new Vector2(_boxCollider.offset.x ,Mathf.Ceil(_spriteRenderer.size.y / 2));
     }
 
     public void PlaceAtPosition(Vector2 position)
     {
+        Vector2 size = _spriteRenderer.size;
         // add enough to place background based on fact we expand Background from the middle
-        Vector2 size = spriteRenderer.size;
         Vector2 sizeRounded = new Vector2(0, Mathf.Ceil(size.y / 2));
         transform.position = position + sizeRounded;
     }
--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -6,47 +6,59 @@ using UnityEngine.Tilemaps;
 
 public class LevelGenerator : MonoBehaviour
 {
-    private readonly int leftMostX = -5;
-    private readonly int rightMostX = 4;
-    private readonly int bottomY = 1;
+    private readonly int _leftMostX = -5;
+    private readonly int _rightMostX = 4;
+    private readonly int _bottomY = 1;
 
     [SerializeField]
-    private int minimumPlatformWidth = 3;
-    private int currentPlayerSection = 0;
-    private int platformSize = 5;
-    public int sectionHeight = 20;
+    private int _minimumPlatformWidth = 3;
+    private int _currentPlayerSection = 0;
+    private int _platformSize = 5;
+
+    // Keep sectionHeight and tilesetSwitchDivider as values possible to divide (section/tilesetSwitchDivider)
     [SerializeField]
-    private int loadSectionHeight = 20;
-    private int currentLoadedSections = 0;
-    public int startSectionLevels = 20;
-    private int highestFloorReached;
+    private int _sectionHeight = 20;
+    [SerializeField]
+    private int _loadSectionHeight = 20;
+    private int _currentLoadedSectionsMaxY = 0;
+    public int _startSectionLevels = 20;
+    private int _highestFloorReached;
+    private int _oldPlayerY;
+
+    [SerializeField]
+    private int _platformShrinkMagnitude = 2;
+    [SerializeField]
+    private int _cameraSpeedupMagnitude = 3;
+    [SerializeField]
+    private float _cameraSpeedUp = 0.2f;
+    [SerializeField]
+    private int _tilesetSwitchDivider = 100; // After how many levels tiles change
 
 
-    private GameObject player;
-    private RuleTile currentRuleBrush;
-    public RuleTile[] platformRuleTiles;
-    public RuleTile wallRuleTile;
-    public Tilemap platformTilemap;
-    public Tilemap wallsTilemap;
-    public Tilemap collisionWallsTilemap;
-    public GameObject levelIndicator;
-    public GameObject highestLevelReachedAsset;
-    public GameObject playerCamera;
+    private GameObject _player;
+    private RuleTile _currentRuleBrush;
+    public RuleTile[] PlatformRuleTiles;
+    public RuleTile WallRuleTile;
+    public Tilemap PlatformTilemap;
+    public Tilemap WallsTilemap;
+    public Tilemap CollisionWallsTilemap;
+    public GameObject LevelIndicator;
+    public GameObject HighestLevelReachedAsset;
+    public GameObject PlayerCamera;
 
     public GameObject Background;
 
     [SerializeField]
     private GameObject CoinObject;
 
-    private int oldPlayerY;
-
     void Start()
     {
-        currentRuleBrush = platformRuleTiles[0];
-        player = GameObject.FindWithTag("Player");
-        oldPlayerY = Mathf.RoundToInt(playerCamera.transform.position.y);
-        highestFloorReached = GameManager.Instance.HighestFloorReached;
-        currentLoadedSections = 0;
+        _currentRuleBrush = PlatformRuleTiles[0];
+        _currentPlayerSection = 0;
+        _player = GameObject.FindWithTag("Player");
+        _oldPlayerY = Mathf.RoundToInt(PlayerCamera.transform.position.y);
+        _highestFloorReached = GameManager.Instance.HighestFloorReached;
+        _currentLoadedSectionsMaxY = 0;
         GeneratePlatforms();
     }
 
@@ -57,37 +69,33 @@ public class LevelGenerator : MonoBehaviour
 
     private void UpdatePlatformsBasedOnPlayerPosition()
     {
-        if (player != null)
+        if (_player != null)
         {
-            int playerY = Mathf.RoundToInt(playerCamera.transform.position.y);
-            if (oldPlayerY < playerY && playerY > currentLoadedSections)
+            int playerCameraY = Mathf.RoundToInt(PlayerCamera.transform.position.y);
+
+            if (playerCameraY > _oldPlayerY && playerCameraY > (_currentLoadedSectionsMaxY - _loadSectionHeight))
             {
-                if (IsPlatformsLoadNeeded(playerY))
+                if (IsPlatformsLoadNeeded(playerCameraY))
                 {
                     //workswonky for some reason...
-                    int fromLevel = (loadSectionHeight + 1) + loadSectionHeight * (currentPlayerSection);
-                    int toLevel = (loadSectionHeight) + loadSectionHeight * (currentPlayerSection + 1);
-                    GeneratePlatforms(fromLevel , toLevel );
-                    currentLoadedSections = toLevel;
-                    if (IsPlayerSectionIncreaseNeeded(playerY))
+                    int fromLevel = _currentLoadedSectionsMaxY + 1;
+                    int toLevel = fromLevel + _loadSectionHeight;
+                    
+                    if (IsPlayerSectionIncreaseNeeded(playerCameraY))
                     {
-                        currentPlayerSection++;
+                        _currentPlayerSection++;                        
+
                     }
-
-                    if (IsRuleBrushSwitchNeeded())
-                    {
-                        currentRuleBrush = platformRuleTiles[currentPlayerSection];
-                    }
-
-                    if (IsPlatfromSizeShrinkNeeded())
-                        platformSize--;
-
+                    
                     if (IsCameraSpeedIncreaseNeeded())
                     {
-                        playerCamera.GetComponent<FollowCamera>().cameraSpeed += 0.2f;
+                        PlayerCamera.GetComponent<FollowCamera>().cameraSpeed += _cameraSpeedUp;
+                        Debug.Log("Camera Speed: " + PlayerCamera.GetComponent<FollowCamera>().cameraSpeed);
                     }
+
+                    GeneratePlatforms(fromLevel , toLevel );
                 }
-                oldPlayerY = playerY;
+                _oldPlayerY = playerCameraY;
 
                 CleanupGrid();
             }
@@ -99,72 +107,97 @@ public class LevelGenerator : MonoBehaviour
     {
         // clean everything from below current section
 
-        for (int i = (currentPlayerSection-1) * sectionHeight; i > 0; i--)
+        for (int i = (_currentPlayerSection-1) * _sectionHeight; i > 0; i--)
         {
-            for (int j = leftMostX - 6; j < leftMostX; j++)
+            for (int j = _leftMostX - 6; j < _leftMostX; j++)
             {
-                wallsTilemap.SetTile(new Vector3Int(j, i, 0), null);
+                WallsTilemap.SetTile(new Vector3Int(j, i, 0), null);
             }
-            for (int j = leftMostX; j < rightMostX +1; j++)
+            for (int j = _leftMostX; j < _rightMostX +1; j++)
             {
-                platformTilemap.SetTile(new Vector3Int(j, i, 0), null);
+                PlatformTilemap.SetTile(new Vector3Int(j, i, 0), null);
             }
-            for (int j = rightMostX + 1; j < rightMostX + 7; j++)
+            for (int j = _rightMostX + 1; j < _rightMostX + 7; j++)
             {
-                wallsTilemap.SetTile(new Vector3Int(j, i, 0), null);
+                WallsTilemap.SetTile(new Vector3Int(j, i, 0), null);
             }
         }
     }
 
     private bool IsCameraSpeedIncreaseNeeded()
     {
-        return currentPlayerSection % 3 == 0;
+        return _currentPlayerSection % _cameraSpeedupMagnitude == 0 && (_currentPlayerSection * _sectionHeight) % _tilesetSwitchDivider == 0;
     }
 
-    private bool IsPlatfromSizeShrinkNeeded()
+    private bool IsPlatfromSizeShrinkNeeded(int playerY)
     {
-        return platformSize >= minimumPlatformWidth && currentPlayerSection % 5 == 0;
+        //return _currentPlayerSection >= (_sectionHeight / _tilesetSwitchDivider) && playerY % (_sectionHeight * _currentPlayerSection) == 0 && _platformSize >= _minimumPlatformWidth && _currentPlayerSection % _platformShrinkMagnitude == 0 /*&& (_currentPlayerSection * _sectionHeight) % _tilesetSwitchDivider == 0*/;
+        return _platformSize >= _minimumPlatformWidth && _currentPlayerSection >= (_sectionHeight / _tilesetSwitchDivider) && _currentPlayerSection % _platformShrinkMagnitude == 0 /*&& (_currentPlayerSection * _sectionHeight) % _tilesetSwitchDivider == 0*/;
     }
 
     private bool IsPlayerSectionIncreaseNeeded(int playerY)
     {
-        return playerY > (currentPlayerSection * sectionHeight);
+        return playerY > ((_currentPlayerSection + 1) * _sectionHeight);
     }
 
-    private bool IsRuleBrushSwitchNeeded()
+    private bool IsRuleBrushSwitchNeeded(int y)
     {
-        return currentPlayerSection >= 1 && currentPlayerSection < platformRuleTiles.Length;
+        return _currentPlayerSection >= (_tilesetSwitchDivider/_sectionHeight) && Mathf.CeilToInt(_currentPlayerSection / (_tilesetSwitchDivider / _sectionHeight)) < PlatformRuleTiles.Length && y % _tilesetSwitchDivider == 0;
     }
 
+    /// <summary>
+    /// Load happens when _player Y position is larger than halfway through _loadSectionHeight for latest loadSection
+    /// </summary>
+    /// <param name="playerY"></param>
+    /// <returns></returns>
     private bool IsPlatformsLoadNeeded(int playerY)
     {
-        return playerY > (currentPlayerSection * sectionHeight - (loadSectionHeight / 2));
+        return playerY > (_currentLoadedSectionsMaxY - _loadSectionHeight);
     }
 
     private void GeneratePlatforms()
     {
-        GeneratePlatforms(bottomY, loadSectionHeight);
+        GeneratePlatforms(_bottomY, _loadSectionHeight);
     }
 
     private void GeneratePlatforms(int fromY,int toY)
     {
+        PlaceBackground(fromY);        
         for (int y = fromY; y <= toY; y++)
         {
-            PlaceWalls(leftMostX - 6, leftMostX, y);
+            PlaceWalls(_leftMostX - 6, _leftMostX, y);
+
+            if (IsRuleBrushSwitchNeeded(y))
+            {
+                int divider = _tilesetSwitchDivider / _sectionHeight;
+                int brushNr = Mathf.FloorToInt(_currentPlayerSection / divider);
+                _currentRuleBrush = PlatformRuleTiles[brushNr];
+            }
+            
+            if (y > 0 && y % (_sectionHeight) == 0)
+            {
+                if (IsPlatfromSizeShrinkNeeded(y))
+                {
+                    _platformSize--;
+                    Debug.Log("Platform Size: " + _platformSize);
+                }
+            }
 
             // Section division 
-            if (y > 0 && y % (sectionHeight) == 0)
+
+            if (y > 0 && y % (_sectionHeight) == 0)
             {
                 PlaceSectionDivision(y);
+                
             }
             else
             {
                 PlaceRandomPlatforms(y);
             }
             PlaceCoins(y);
-            PlaceWalls(rightMostX + 1, rightMostX + 7, y);            
+            PlaceWalls(_rightMostX + 1, _rightMostX + 7, y);            
         }
-        PlaceBackground(fromY);        
+        _currentLoadedSectionsMaxY = toY;
     }
 
     private void PlaceCoins(int y)
@@ -181,7 +214,7 @@ public class LevelGenerator : MonoBehaviour
         }
 
         int spot = 0;
-        for (int x = leftMostX; x <= rightMostX; x ++)
+        for (int x = _leftMostX; x <= _rightMostX; x ++)
         {
             if (coinMask[spot] != 0)
             {
@@ -213,24 +246,24 @@ public class LevelGenerator : MonoBehaviour
     {
         for (int x = from; x < to; x++)
         {
-            wallsTilemap.SetTile(new Vector3Int(x, y, 0), wallRuleTile);
+            WallsTilemap.SetTile(new Vector3Int(x, y, 0), WallRuleTile);
         }
     }
 
     private void PlaceSectionDivision(int y)
     {
-        for (int x = leftMostX; x <= rightMostX; x++)
+        for (int x = _leftMostX; x <= _rightMostX; x++)
         {
-            platformTilemap.SetTile(new Vector3Int(x, y, 0), currentRuleBrush);
+            PlatformTilemap.SetTile(new Vector3Int(x, y, 0), _currentRuleBrush);
             PlaceLevelIndicator(y, x);
         }
     }
 
     private void PlaceLevelIndicator(int y, int x)
     {
-        if (x == (leftMostX + rightMostX) / 2 && y > 0)
+        if (x == (_leftMostX + _rightMostX) / 2 && y > 0)
         {
-            GameObject go = Instantiate(levelIndicator, new Vector3(x, y, 0), Quaternion.identity);
+            GameObject go = Instantiate(LevelIndicator, new Vector3(x, y, 0), Quaternion.identity);
             LevelText lt = go.GetComponent<LevelText>();
             lt.SetText(y.ToString());
         }
@@ -246,27 +279,27 @@ public class LevelGenerator : MonoBehaviour
 
         while (!isPlaceable)
         {
-            leftEdgeOfPlatform = UnityEngine.Random.Range(leftMostX, rightMostX);
+            leftEdgeOfPlatform = UnityEngine.Random.Range(_leftMostX, _rightMostX);
 
-            if (leftEdgeOfPlatform+platformSize <= rightMostX)
+            if (leftEdgeOfPlatform+_platformSize <= _rightMostX)
             {
                 isPlaceable = true;
             }
         }
 
-        for (int x = leftEdgeOfPlatform; x <= leftEdgeOfPlatform + platformSize; x += 1)
+        for (int x = leftEdgeOfPlatform; x <= leftEdgeOfPlatform + _platformSize; x += 1)
         {
             PlaceHighestLevelReachedSign(y, leftEdgeOfPlatform, x);
-            platformTilemap.SetTile(new Vector3Int(x, y, 0), currentRuleBrush);
+            PlatformTilemap.SetTile(new Vector3Int(x, y, 0), _currentRuleBrush);
         }
     }
 
     private void PlaceHighestLevelReachedSign(int y, int leftEdgeOfPlatform, int x)
     {
-        if (y == highestFloorReached && x == leftEdgeOfPlatform + 1)
+        if (y == _highestFloorReached && x == leftEdgeOfPlatform + 1)
         {
             // for now place flag for highest reached floor on second from left
-            GameObject go = Instantiate(highestLevelReachedAsset, new Vector3(x, y + 1, 0), Quaternion.identity); // +1 for y, because of difference between grid and coord system
+            GameObject go = Instantiate(HighestLevelReachedAsset, new Vector3(x, y + 1, 0), Quaternion.identity); // +1 for y, because of difference between grid and coord system
             LevelText lt = go.GetComponent<LevelText>();
             lt.SetText(y.ToString());
         }
